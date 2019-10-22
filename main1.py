ee7bc19391e4167ee283d395ff63224e5cd5d0d4
Ricardo Taina <ricardo.taina@sjsu.edu>
2019-10-21 20:59:50 -0700

    Added user DataBase in main

--- main1.py
+++ main1.py
@@ -2,6 +2,34 @@ from flask import Flask
 from flask import render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from User import User
+
+userList = []
+
+userDataFile = open("userData.txt","r+")
+
+counter = 0
+
+for line in userDataFile:
+
+    fields = line.split(";")
+    field1 = fields[0]
+    #print (field1)
+    field2 = fields[1]
+    #print (field2)
+    field3 = fields[2]
+    #print (field3)
+
+    temp = User(field1,field2,field3)
+
+    userList.append(temp)
+
+userDataFile.close()
+
+for User in userList:
+
+    print (User.username)
+
 
 # uncomment line below once you have created the
 # TopCities class inside the form.py file
