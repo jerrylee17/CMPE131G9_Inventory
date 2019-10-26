cd9a84b4e22bc068f0f012c883aa73f9f794a75d
Ricardo Taina <ricardo.taina@sjsu.edu>
2019-10-25 22:16:16 -0700

    validate registration + flash message

--- app/Register.py
+++ app/Register.py
@@ -3,28 +3,40 @@ from flask import render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
 from app.obj.User import User
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, EqualTo
+from flask import flash, redirect , request
 
 app.config['SECRET_KEY'] = 'some-key'
 
-@app.route('/registration')
+@app.route('/registration',methods = ["GET","POST"])
 
 def register():
 
     form = registration()
 
+    #if form.validate_on_submit():
+
+    #  print("entered if")
+        
+    #  return redirect(url_for('login'))
+
+    if form.validate_on_submit():
+        flash("Succesfull Registration")
+        return redirect("/login")
+   
     return render_template('registerpage.html',form = form)
-
-
+    
 class registration(FlaskForm):
 
-    username = StringField("Username",validators=[DataRequired()])
+    #print("entered registration")
+    
+    username = StringField("Username",validators=[DataRequired(),Length(max=10)])
 
-    password = PasswordField("Password",validators=[DataRequired()])
+    password = PasswordField("Password",validators=[DataRequired(),Length(max=10)])
 
-    passwordVer = PasswordField("Password_Verification",validators=[DataRequired()])
+    passwordVer = PasswordField("Password Verification",validators=[DataRequired(),EqualTo("password","password does not match")])
 
-    submit = SubmitField("Sign In")
+    submit = SubmitField("Register")
 
 if __name__ == '__main__':
     app.run()
